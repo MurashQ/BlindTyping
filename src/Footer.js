4dbb44cd2646395fa3cc9dffb308e04273f89788
MurashQ <semen-fofanov213@yandex.ru>
2023-06-10 21:20:48 +0400

    added README.md

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
-
-function Footer(props) {
-  return (
-    <footer>
-      <div className="footer-info">
-        <h2>Информация</h2>
-        <p>Русский текст сгенерирован на <a href="https://fish-text.ru" rel="noreferrer" target="_blank">fish-text.ru</a></p>
-        <p>Английские слова сгенерированы на <a href="https://random-word-api.herokuapp.com" rel="noreferrer" target="_blank">random-word-api.herokuapp.com</a></p>
-      </div>
-      <div className="footer-links">
-        <h2>Мои ссылки</h2>
-        <p>Исходники приложения <a href="https://github.com/MurashQ/BlindTyping" rel="noreferrer" target="_blank">git</a></p>
-      </div>
-    </footer>
-    );
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="footer-info">
+          <h2>Информация</h2>
+          <p>Русский текст сгенерирован на <a href="https://fish-text.ru" rel="noreferrer" target="_blank">fish-text.ru</a></p>
+          <p>Английские слова сгенерированы на <a href="https://random-word-api.herokuapp.com" rel="noreferrer" target="_blank">random-word-api.herokuapp.com</a></p>
+        </div>
+        <div className="footer-links">
+          <h2>Мои ссылки</h2>
+          <p>Исходники приложения <a href="https://github.com/MurashQ/BlindTyping" rel="noreferrer" target="_blank">git</a></p>
+        </div>
+      </footer>
+      );
+  }
 }
 export default Footer;
